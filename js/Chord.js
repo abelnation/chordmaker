b7072e92091c105491e5a42c882fbd025749ff4b
Abel Allison <abel.allison@gmail.com>
2012-07-22 00:23:02 -0700

    good bug fixed with the circles causing them not to transform.

--- js/Chord.js
+++ js/Chord.js
@@ -78,7 +78,7 @@ Chord = function(_element, _config) {
 
     // Rotate Horizontally
     var transform_str = "r-90,"+self.config.grid_x+","+self.config.grid_y+"t-"+grid_height+",0";
-    console.log(transform_str);
+    //console.log(transform_str);
     //self.neck.transform(transform_str);
     self.notes.forEach(function(e) {
       //e.transform(transform_str);
@@ -97,10 +97,41 @@ Chord = function(_element, _config) {
     for (i = 1; i < wv; i++) {
         path = path.concat(["M", Math.round(x + i * columnWidth) + .5, Math.round(y) + .5, "V", Math.round(y + h) + .5]);
     }
+
+    self.grid_back = r.rect(self.config.grid_x, self.config.grid_y, grid_width, grid_height).attr("fill", "white");
     self.grid = r.path(path.join(",")).attr({stroke: color, 'stroke-width': self.config.grid_stroke_width})
     self.neck.push(
-      self.grid
+      self.grid,
+      self.grid_back
     );
+
+    // TODO: Start Here
+    //       Hacky code to do a cursor and to add notes.
+    //       Needs to be hella formalized
+    self.grid_back.mousemove(_.bind(function(e) {
+      var mouse_grid_x = e.offsetX - self.config.grid_x;
+      var mouse_grid_y = e.offsetY - self.config.grid_y;
+
+      var closest_string = Math.round(mouse_grid_x / self.config.string_gap);
+      var closest_fret = Math.ceil(mouse_grid_y / self.config.fret_gap);
+      //console.log(""+closest_string+","+closest_fret);
+
+      if (closest_string != self.cursor_note_string ||
+          closest_fret   != self.cursor_note_fret) {
+        self.cursor_note_string = closest_string;
+        self.cursor_note_fret = closest_fret;
+        if (self.cursor_note) {
+          self.cursor_note.remove();
+        }
+        self.cursor_note = drawNote({
+          fret: self.cursor_note_fret,
+          string: self.cursor_note_string
+        });
+        self.cursor_note.click(function() {
+          self.cursor_note = null;
+        });
+      }
+    }, self));
   };
 
   var drawNotes = function() {
@@ -150,11 +181,12 @@ Chord = function(_element, _config) {
     var class_str = "chord-note";
     if (note.class) { class_str = class_str + " " + note.class; }
 
-    var glyph = r.circle(x,y,self.config.note_radius)
-      .attr(note_style)
-      .node.setAttribute("class", class_str);
+    var glyph = r.circle(x,y,self.config.note_radius);
+    glyph.node.setAttribute("class", class_str);
+    glyph.attr(note_style);
 
     self.notes.push(glyph);
+    return glyph;
   };
 
   var drawMuteStringAnnotatation = function(string) {
