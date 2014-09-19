608c25548ac201300129a28646e42ea7d7e08cb3
aallison <aallison@yahoo-inc.com>
2014-09-19 00:15:39 -0700

    TuningFactory updated to new scope scheme

--- src/js/TuningFactory.js
+++ src/js/TuningFactory.js
@@ -1,29 +1,40 @@
-/* global Tuning */
+/* global _, Chords */
 
-function TuningFactory() {
-  throw Error("Cannot instantiate TuningFactory.");
-}
+(function(exports) {
+  var Tuning = exports.Tuning;
 
-TuningFactory.fromInstrument = function(instrument, tuning_type) {
-  if (!instrument || !_.isString(instrument)) {
-    throw TypeError("Must provide an instrument string");
-  }
-  instrument = instrument.toLowerCase();
-  
-  if (!_.has(Tuning.instruments, instrument)) {
-    throw TypeError("Invalid instrument");
-  }
+  var TuningFactory = function TuningFactory() {
+    throw Error("Cannot instantiate TuningFactory.");
+  };
 
-  if (!tuning_type) {
-    tuning_type = "default";
-  } else if (!_.isString(tuning_type)) {
-    throw TypeError("tuning_type must be a string");
-  }
-  
-  tuning_type = tuning_type.toLowerCase();
-  if (!_.has(Tuning.instruments[instrument], tuning_type)) {
-    throw TypeError("Invalid tuning type: " + tuning_type);
-  }
+  _.extend(TuningFactory, {
+    fromInstrument: function(instrument, tuning_type) {
+      if (!instrument || !_.isString(instrument)) {
+        throw TypeError("Must provide an instrument string");
+      }
+      instrument = instrument.toLowerCase();
 
-  return new Tuning(Tuning.instruments[instrument][tuning_type]);
-};
+      if (!_.has(Tuning.instruments, instrument)) {
+        throw TypeError("Invalid instrument");
+      }
+
+      if (!tuning_type) {
+        tuning_type = "default";
+      } else if (!_.isString(tuning_type)) {
+        throw TypeError("tuning_type must be a string");
+      }
+
+      tuning_type = tuning_type.toLowerCase();
+      if (!_.has(Tuning.instruments[instrument], tuning_type)) {
+        throw TypeError("Invalid tuning type: " + tuning_type);
+      }
+
+      return new Tuning(Tuning.instruments[instrument][tuning_type]);
+    },
+  });
+
+  _.extend(exports, {
+    TuningFactory: TuningFactory
+  });
+
+})(Chords);
